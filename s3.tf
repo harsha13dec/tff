f042a286189a1dae2c603ca9b0260522116b74eb
harsha13dec <105868260+harsha13dec@users.noreply.github.com>
2022-08-11 21:28:28 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "bucklebuck" {
   bucket = "my-tf-massiveprince-bucket11"
-  acl    = "public"
+  acl    = "private"
 
   tags = {
     Name        = "rav1i-day1a-santhos1h-deepak1231"
